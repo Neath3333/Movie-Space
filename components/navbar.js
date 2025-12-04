ca4e694afad1115149863a53127a5ec6155f4fbb
Neath333 <daneath@saerosoft.com>
2025-12-04 07:58:32 +0700

    feat: create navbat

---
+++ components/navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>My Website</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/discover">Discover</a></li>
+        <li><a href="/review">Reviews</a></li> 
+        <li><a href="/recommendation">Recommendation</a></li>
+        <li><a href="/setting">Setting</a></li>
+        
+      </ul>
+    </nav>
+  );  
+}   
