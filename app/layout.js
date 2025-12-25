0f9225e47f3f056e41f75956492e4928d162f405
Neath3333 <daneath@saerosoft.com>
2025-12-25 23:29:50 +0700

    feat: add navbar and footer to Layout page

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,17 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
-export default function RootLayout({
-}) {
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import React from "react";
+export default function RootLayout({children}) {
   return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
   );
 }
