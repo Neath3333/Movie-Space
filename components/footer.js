4aa6c4e08669e5776525c1a3fe7fbb076a0771cb
Neath3333 <daneath@saerosoft.com>
2026-01-01 23:58:21 +0700

    style: redesign the footer

--- components/footer.js
+++ components/footer.js
@@ -8,16 +8,18 @@ export default function Footer() {
         <div  className="">
             <Image src="/" alt="logo" width={100} height={100} />
         </div>
-        <div className="text-blue-600">
+        <div className="text-blue-600 flex flex-col items-center justify-center p-4">
             <h2>Quick Links</h2>
-            <ul >
+            <ul className="flex flex-col items-center justify-center space-y-2 mb-4">
                 <li><Link href="/about">About Us</Link></li>
                 <li><Link href="/discover">Discover</Link></li>
                 <li><Link href="/privacy">Privacy Policy</Link></li>
                 <li><Link href="/terms">Terms of Service</Link></li>
                 <li><Link href="/settings">Settings</Link></li>
             </ul>
+            <div className="text-gray-600">
         <p>© 2024 My Website. All rights reserved.</p>
+        </div>
         </div>
     </footer>
   );
