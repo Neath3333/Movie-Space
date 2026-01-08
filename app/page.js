da7225e1195f35ccab2364d48c958ac060d99615
Neath3333 <daneath@saerosoft.com>
2026-01-09 00:00:31 +0700

    feat(home):add moviecard in home page

--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
+import MovieCard from '../components/MovieCard';
 
 
 export default function Home() {
 
 return (
     <div className='bg-blue-900'>
-   
+   <MovieCard  />
     </div>
   );
 }
