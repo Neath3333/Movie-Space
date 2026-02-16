f0d9f606ec340d4182260c46970298d28e66c6ec
Neath3333 <soda.neat@gmail.com>
2026-02-16 23:57:43 +0700

    fix: rename movieCard to MovieCard for import compatibility

---
+++ components/theme.js
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react';
+
+export default function ThemeProvider() {
+    return (
+        <div className="theme-provider">
+          <script>
+            
+          </script>
+        </div>
+    );
+}
