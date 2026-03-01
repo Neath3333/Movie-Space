a52c39c88e895e728bcf1a74dc3def881655836b
Neath3333 <soda.neat@gmail.com>
2026-03-01 23:56:33 +0700

    fix: improve search functionality with Enter key and loading state

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,12 +1,49 @@
 'use client';
 import React from "react";
 import SearchIcon from '@mui/icons-material/Search';
+import { searchMovies } from "@/lib/tmdb";
 
 export default function SearchBar(){
+    const [searchQuery, setSearchQuery] = React.useState("");
+    const [searchResults, setSearchResults] = React.useState([]);
+    const [showResults, setShowResults] = React.useState(false); 
+    const [isLoading, setIsLoading] = React.useState(false);
+    async function handleSearch(){
+        setIsLoading(true);
+        const results = await searchMovies(searchQuery);
+        setSearchResults(results.results);
+        setShowResults(true);
+        setIsLoading(false);
+    }
     return(
         <div className="relative">
-            <input type="text" placeholder="Search titles...." className="py-2 px-5 ml-15 rounded-xl bg-white focus:outline-none" />
-            <SearchIcon className="ml-2 absolute right-3 text-gray-500 top-2 cursor-pointer"/>
+            <input 
+            value={searchQuery} 
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyDown={(e)=> {
+                if (e.key === 'Enter'){
+                    handleSearch();
+                }
+            }}
+            
+            type="text" 
+            placeholder="Search titles...." 
+            className="py-2 px-5 ml-15 rounded-xl bg-white focus:outline-none" 
+            />
+            <SearchIcon onClick={handleSearch} className="ml-2 absolute right-3 text-gray-500 top-2 cursor-pointer"/>
+            {isLoading &&(
+                <div>
+
+                </div>
+            )}
+            {showResults && searchResults.length > 0 && (
+                <div>
+                    <div>
+                        
+                    </div>
+                </div>
+
+            )}
         </div>
     )
 }
