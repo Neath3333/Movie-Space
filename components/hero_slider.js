f8a63e38165b90515e80c9890463270b58638ca7
Neath3333 <soda.neat@gmail.com>
2026-03-25 23:07:57 +0700

    refactor: seperate component and page for hero section

--- components/hero_slider.js
+++ components/hero_slider.js
@@ -1,17 +1,14 @@
-'use client'
-
-// import Image from 'next/image'
-// import {useEffect, useState } from "react"
+import Image from 'next/image'
 import React from 'react'
-import {getUpcomingMovies} from "@/lib/tmdb"; 
-import MovieCard from "@/components/MovieCard";                                                                 
-
-
+import {fetchMovieById, getUpcomingMovies} from "@/lib/tmdb";                                                
 
 export default async function HeroSlider (){
    const hero = await getUpcomingMovies();
+   const movie = await fetchMovieById();
    const heroFetch = hero.results;
-
+  const backdropUrl = hero.backdrop_path
+    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+    : "/public/logo1.jpg";
    return(
     <div>
           <div className="px-5 py-8 grow">
@@ -20,8 +17,10 @@ export default async function HeroSlider (){
                 <div
                   key={`${movie.id}-${index}`}
                   className="text-black text-[18px] hover:scale-105 transition-transform duration-200 p-4xl"
-                >
-                  <MovieCard movie={movie} />
+                > 
+                  <Image 
+                  src={backdropUrl}
+                  />
                 </div>
               ))}
             </div>
