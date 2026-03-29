525f4bd12e6f829820d8bd37e680f41a2b631dd2
Neath3333 <soda.neat@gmail.com>
2026-03-29 19:42:38 +0700

    fix : link logo and project name to redirect to homepage

--- components/hero_slider.js
+++ components/hero_slider.js
@@ -1,30 +1,30 @@
-import Image from 'next/image'
-import React from 'react'
-import {fetchMovieById, getUpcomingMovies} from "@/lib/tmdb";                                                
+// import Image from 'next/image'
+// import React from 'react'
+// import {fetchMovieById, getUpcomingMovies} from "@/lib/tmdb";                                                
 
-export default async function HeroSlider (){
-   const hero = await getUpcomingMovies();
-   const movie = await fetchMovieById();
-   const heroFetch = hero.results;
-  const backdropUrl = hero.backdrop_path
-    ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
-    : "/public/logo1.jpg";
-   return(
-    <div>
-          <div className="px-5 py-8 grow">
-            <div className="grid grid-cols-5 gap-4 p-4 container mx-auto px-2 py-8 grow">
-              {heroFetch.map((movie, index) => (
-                <div
-                  key={`${movie.id}-${index}`}
-                  className="text-black text-[18px] hover:scale-105 transition-transform duration-200 p-4xl"
-                > 
-                  <Image 
-                  src={backdropUrl}
-                  />
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-   )
-}
+// export default async function HeroSlider (){
+//    const hero = await getUpcomingMovies();
+//    const movie = await fetchMovieById();
+//    const heroFetch = hero.results;
+//   const backdropUrl = hero.backdrop_path
+//     ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
+//     : "/public/logo1.jpg";
+//    return(
+//     <div>
+//           <div className="px-5 py-8 grow">
+//             <div className="grid grid-cols-5 gap-4 p-4 container mx-auto px-2 py-8 grow">
+//               {heroFetch.map((movie, index) => (
+//                 <div
+//                   key={`${movie.id}-${index}`}
+//                   className="text-black text-[18px] hover:scale-105 transition-transform duration-200 p-4xl"
+//                 > 
+//                   <Image 
+//                   src={backdropUrl}
+//                   />
+//                 </div>
+//               ))}
+//             </div>
+//           </div>
+//         </div>
+//    )
+// }
--- components/navbar.js
+++ components/navbar.js
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import SearchBar from "./SearchBar";
 import React from "react";
 import AuthButton from "./auth-button";
 
 const navLinks = [
-  { name: 'Home', href: '/' },
-  { name: 'Discover', href: '/discover' },
-  { name: 'Reviews', href: '/review' },
-  { name: 'Recommendation', href: '/recommendation' },
-  { name: 'Setting', href: '/setting' }, // Dropdown
+  { name: "Home", href: "/" },
+  { name: "Discover", href: "/discover" },
+  { name: "Reviews", href: "/review" },
+  { name: "Recommendation", href: "/recommendation" },
+  { name: "Setting", href: "/setting" }, // Dropdown
 ];
 
 export default function Navbar() {
@@ -20,22 +20,26 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 z-50 w-full flex p-6 bg-[#FFB7C5]/70 text-black items-center justify-center ">
-      <Image src="/logo.png" alt="logo" width={100} height={100} />
-      <h1 className="mr-20 pl-8 text-xl font-bold">MOVIE SPACE</h1>
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={100} height={100} />
+      </Link>
+      <Link href="/">
+        <h1 className="mr-20 pl-8 text-xl font-bold">MOVIE SPACE</h1>
+      </Link>
 
       <ul className="flex space-x-8">
         {navLinks.map((link) => {
           const isActive = pathname === link.href;
 
           // Settings dropdown
-          if (link.name === 'Setting') {
+          if (link.name === "Setting") {
             return (
               <li key={link.href} className="relative group">
                 <span
                   className={
                     isActive
-                      ? 'text-lime-800 font-bold border-b-2 border-lime-600 cursor-pointer'
-                      : 'hover:text-lime-200 transition-colors cursor-pointer'
+                      ? "text-lime-800 font-bold border-b-2 border-lime-600 cursor-pointer"
+                      : "hover:text-lime-200 transition-colors cursor-pointer"
                   }
                 >
                   {link.name}
@@ -43,16 +47,28 @@ export default function Navbar() {
 
                 {/* Dropdown */}
                 <div className="absolute right-0 mt-2 w-48 bg-white text-black rounded shadow-lg opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-200">
-                  <Link href="/profiles" className="block px-2 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/profiles"
+                    className="block px-2 py-2 hover:bg-gray-200"
+                  >
                     Profile
                   </Link>
-                  <Link href="/setting/privacy" className="block px-2 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/setting/privacy"
+                    className="block px-2 py-2 hover:bg-gray-200"
+                  >
                     Ratings & Watchlist
                   </Link>
-                  <Link href="/theme" className="block px-2 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/theme"
+                    className="block px-2 py-2 hover:bg-gray-200"
+                  >
                     Theme
                   </Link>
-                  <Link href="/about" className="block px-2 py-2 hover:bg-gray-200">
+                  <Link
+                    href="/about"
+                    className="block px-2 py-2 hover:bg-gray-200"
+                  >
                     About
                   </Link>
                 </div>
@@ -67,8 +83,8 @@ export default function Navbar() {
                 href={link.href}
                 className={
                   isActive
-                    ? 'text-lime-400 font-bold border-b-2 '
-                    : 'hover:text-lime-200 transition-colors'
+                    ? "text-lime-400 font-bold border-b-2 "
+                    : "hover:text-lime-200 transition-colors"
                 }
               >
                 {link.name}
@@ -77,10 +93,10 @@ export default function Navbar() {
           );
         })}
       </ul>
-     <SearchBar/>
-     <div className="ml-8">
-    <AuthButton />
-    </div>
+      <SearchBar />
+      <div className="ml-8">
+        <AuthButton />
+      </div>
     </nav>
   );
 }
