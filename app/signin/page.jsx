ab85f2289b873bf78a6d10f9a922ec2b98c16193
Neath3333 <soda.neat@gmail.com>
2026-04-16 23:39:56 +0700

    fix: sign in page authenticate with google account

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -1,18 +1,15 @@
 'use client';
 import React from "react";
-// import { signIn } from "next-auth/react";
-import { useState } from "react";
+import { signIn } from "next-auth/react";
 
 export default function SignInPage() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const handleGoogleSignIn = async () => {
+        await signIn("google", { callbackUrl: "/" });
+    };
+
     return (
         <div className="">
-         <form>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input value={password} onChange={(e) => setPassword(e.target.value)}/>
-            <button>Login</button>
-         </form>
+            <button onClick={handleGoogleSignIn}>Sign in with Google </button> 
          </div>
     )
 };
