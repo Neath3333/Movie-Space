eed758576c339d9a22ac952b47a6930e8022cc49
Neath3333 <soda.neat@gmail.com>
2026-04-19 16:42:20 +0700

    fix : display google logged profile in profile page.

--- app/profiles/page.jsx
+++ app/profiles/page.jsx
@@ -1,14 +1,26 @@
+'use client'
 import Profile from "../../components/profile"; 
 import React from "react";
+import {useSession} from "next-auth/react"
 
 export default function ProfilePage() {
+    const {data: session , status} = useSession();
+    if ( status === "loading"){
+        return <div>Loading...</div>;
+    }
+    if(!session){
+        return <div>Please sign in to view your profile</div>;
+    }
     const user = {
-        name: "John Doe",
-        imageUrl: "https://randomuser.me/api/portraits/men/32.jpg",
+        name: session.user?.name || "User" , 
+        imageUrl : session.user?.image || "https://randomuser.me/api/potraits/men/32.jpg"    
     }
     return (
         <div className="p-4">
             <Profile name={user.name} imageUrl={user.imageUrl} />
+            <div className = "mt-4">
+                <p>Email: {session.user?.email}</p>
+            </div>
         </div>
     );
 }
