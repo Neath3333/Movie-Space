8863b53084b88500c77f3c7b179e6ba16f751050
Neath3333 <soda.neat@gmail.com>
2026-04-21 23:33:08 +0700

    style: redesign sign up page

--- app/signin/page.jsx
+++ app/signin/page.jsx
@@ -8,8 +8,14 @@ export default function SignInPage() {
     };
 
     return (
-        <div className="">
-            <button onClick={handleGoogleSignIn}>Sign in with Google </button> 
+         <div className="min-h-screen bg-[#0e0e0f] flex items-center justify-center px-4 relative overflow-hidden">
+      <div className="absolute top-[-80px] right-[-60px] w-[300px] h-[300px] rounded-full bg-indigo-600 opacity-10 pointer-events-none" />
+      <div className="absolute bottom-[-40px] left-[-40px] w-[200px] h-[200px] rounded-full bg-violet-700 opacity-10 pointer-events-none" />
+
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleGoogleSignIn}>
+                Sign in with Google
+            </button>
          </div>
+
     )
 };
