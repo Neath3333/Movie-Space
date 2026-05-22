2586388299eccfe71e0fe42acfd616551e8bebb2
Neath3333 <soda.neat@gmail.com>
2026-05-22 19:28:11 +0700

    chore: remove prisma.config.ts and update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,12 @@ import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs,jsx}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { globals: {...globals.browser, ...globals.node} }, 
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    ignores: [".next/**", "node_modules/**", "*.config.js", "*.config.mjs"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: {...globals.browser, ...globals.node} },
     rules: { "react/prop-types": "off" }
   },
   pluginReact.configs.flat.recommended,
